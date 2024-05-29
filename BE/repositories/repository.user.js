29258113733043ec67fca6cd32cea76da1acdfaf
wn-l1x <144139330+wn-l1x@users.noreply.github.com>
2024-05-29 23:07:57 +0700

    tambah login

--- BE/repositories/repository.user.js
+++ BE/repositories/repository.user.js
@@ -1,5 +1,8 @@
+const { Console } = require("console");
 const { pool } = require("../db/pgConnect");
-const crypto = require("crypto")
+const crypto = require("crypto");
+
+const { setLoggedInUserId, getLoggedInUserId } = require("../controller/usersession");
 
 async function signup(req, res) {
   const { username, password, email, divisi } = req.body;
@@ -24,24 +27,28 @@ async function signup(req, res) {
       "SELECT * FROM USERS WHERE USERNAME = $1",
       [username]
     );
-    if (duplicate.rows.length>0) {
-      res.status(200).send("Username already exists");
+    if (duplicate.rows.length > 0) {
+      return res.status(200).send("Username already exists");
+    }
+    let divisi_id;
+    if (divisi) {
+      divisi_id = await pool.query(
+        "SELECT DIVISI_ID FROM DIVISI WHERE NAME = $1",
+        [divisi]
+      );
+      if (divisi_id.rows.length === 0) {
+        return res.status(404).send("Divisi Not Found");
+    }
     }
 
     const result = await pool.query(
       "INSERT INTO USERS (USERNAME,PASSWORD,EMAIL) VALUES ($1,$2,$3) RETURNING USER_ID, USERNAME, EMAIL",
       [username, hashedPassword, email]
     );
-    
+
     const userId = result.rows[0].user_id;
 
-    if (divisi) {
-      const divisi_id = await pool.query(
-        "SELECT DIVISI_ID FROM DIVISI WHERE NAME = $1",
-        [divisi]
-      );
-      //console.log(userId);
-      //console.log(divisi_id.rows[0].divisi_id);
+    if (divisi_id) {
       await pool.query(
         "INSERT INTO USER_DIVISI (USER_ID,DIVISI_ID) VALUES($1,$2)",
         [userId, divisi_id.rows[0].divisi_id]
@@ -51,62 +58,47 @@ async function signup(req, res) {
     res.status(200).send(result.rows[0]);
   } catch (error) {
     console.log(error);
-    res.status(500).send("Internal Server Error");
+    return res.status(500).send("Internal Server Error");
   }
 }
 
 async function login(req, res) {
-    const { username, password, email, divisi } = req.body;
-    try {
-      if (!username || !password || !email || !divisi) {
-        return res
-          .status(400)
-          .send("All fields are required: username, password, email, divisi");
-      }
-      let hashedPassword;
-      if (password) {
-        hashedPassword = crypto
-          .createHash("sha256")
-          .update(password)
-          .digest("base64")
-          .slice(0, 32);
-        /* console.log(
+  const { username, password } = req.body;
+  try {
+    if (!username || !password) {
+      return res
+        .status(400)
+        .send("All fields are required: username, password");
+    }
+    let hashedPassword;
+    if (password) {
+      hashedPassword = crypto
+        .createHash("sha256")
+        .update(password)
+        .digest("base64")
+        .slice(0, 32);
+      /* console.log(
           `Password before hash: ${password}\nPassword after hash: ${hashedPassword}`
         ); */
-      }
-      const duplicate = await pool.query(
-        "SELECT * FROM USERS WHERE USERNAME = $1",
-        [username]
-      );
-      if (duplicate.rows.length>0) {
-        res.status(200).send("Username already exists");
-      }
-  
-      const result = await pool.query(
-        "INSERT INTO USERS (USERNAME,PASSWORD,EMAIL) VALUES ($1,$2,$3) RETURNING USER_ID, USERNAME, EMAIL",
-        [username, hashedPassword, email]
-      );
-      
-      const userId = result.rows[0].user_id;
-  
-      if (divisi) {
-        const divisi_id = await pool.query(
-          "SELECT DIVISI_ID FROM DIVISI WHERE NAME = $1",
-          [divisi]
-        );
-        //console.log(userId);
-        //console.log(divisi_id.rows[0].divisi_id);
-        await pool.query(
-          "INSERT INTO USER_DIVISI (USER_ID,DIVISI_ID) VALUES($1,$2)",
-          [userId, divisi_id.rows[0].divisi_id]
-        );
-      }
-  
-      res.status(200).send(result.rows[0]);
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Internal Server Error");
     }
+    result = await pool.query(
+      "SELECT user_id,username,password FROM USERS WHERE USERNAME = $1",
+      [username]
+    );
+    if (result) {
+      if (result.rows[0].password == hashedPassword) {
+        const LoggedInId = result.rows[0].user_id;
+        setLoggedInUserId(LoggedInId);       
+        res.status(200).send(result.rows[0]);
+      } else {
+        res.status(401).send("Invalid Password");
+      }
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
   }
-
-module.exports = { signup };
+}
+module.exports = { signup, login };
