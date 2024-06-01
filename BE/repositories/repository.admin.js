05e1f42f8fe53ec1d695046a3e09c785b1d605b9
wn-l1x <144139330+wn-l1x@users.noreply.github.com>
2024-06-01 12:59:53 +0700

    admin don kali

--- BE/repositories/repository.admin.js
+++ BE/repositories/repository.admin.js
@@ -6,16 +6,16 @@ const {
   setLoggedInUserId,
   getLoggedInUserId,
 } = require("../controller/usersession");
-  
-async function adminLogin (req,res) {
-    const { username, password } = req.body;
+
+async function adminLogin(req, res) {
+  const { username, password } = req.body;
   try {
     if (!username || !password) {
       return res
         .status(400)
         .send("All fields are required: username, password");
     }
-    
+
     let hashedPassword;
     if (password) {
       hashedPassword = crypto
@@ -32,11 +32,11 @@ async function adminLogin (req,res) {
 
     if (result.rows.length > 0) {
       if (result.rows[0].password == hashedPassword) {
-        if (result.rows[0].roles !== 'Admin') {
+        if (result.rows[0].roles !== "Admin") {
           return res.status(403).send("Access denied: Admin only!");
         } else {
           const LoggedInId = result.rows[0].user_id;
-          setLoggedInUserId(req,LoggedInId);
+          setLoggedInUserId(req, LoggedInId);
           res.status(200).send(result.rows[0]);
         }
       } else {
@@ -44,14 +44,14 @@ async function adminLogin (req,res) {
       }
     } else {
       res.status(404).send("User not found");
-    } 
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
   }
 }
 
-async function adminGrant (req,res) {
+async function adminGrant(req, res) {
   const { username } = req.body;
   try {
     if (getLoggedInUserId(req)) {
@@ -60,8 +60,11 @@ async function adminGrant (req,res) {
     } else {
       return res.status(440).send("Login session expired");
     }
-
-    if (result.rows.length === 0 || result.rows[0].roles !== 'Admin') {
+    result = await pool.query("SELECT * FROM USERS WHERE USER_ID = $1", [
+      user_id,
+    ]);
+    console.log(result.rows[0].roles);
+    if (result.rows.length === 0 || result.rows[0].roles !== "Admin") {
       return res.status(403).send("Access denied: Admins only");
     }
 
@@ -75,34 +78,36 @@ async function adminGrant (req,res) {
     );
 
     if (result.rows.length > 0) {
-      await pool.query(
-        "UPDATE USERS SET roles = 'Admin' WHERE username = $1",
-        [username]
-      ); 
+      await pool.query("UPDATE USERS SET roles = 'Admin' WHERE username = $1", [
+        username,
+      ]);
       res.status(200).send(`Role of ${username} updated to Admin`);
     } else {
       res.status(404).send("User not found");
-    }    
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
   }
 }
 
-async function adminRequest (req, res) {
-  try{
+async function adminGetRequest(req, res) {
+  try {
     let user_id;
     if (getLoggedInUserId(req)) {
       user_id = getLoggedInUserId(req);
     } else {
       return res.status(440).send("Login session expired");
     }
-
-    if (result.rows.length === 0 || result.rows[0].roles !== 'Admin') {
+    result = await pool.query("SELECT * FROM USERS WHERE USER_ID = $1", [
+      user_id,
+    ]);
+    console.log(result.rows[0].roles);
+    if (result.rows.length === 0 || result.rows[0].roles !== "Admin") {
       return res.status(403).send("Access denied: Admins only");
     }
 
-    result = await pool.query ("SELECT * FROM KONTEN");
+    result = await pool.query("SELECT * FROM KONTEN");
     res.status(200).send(result.rows);
   } catch (error) {
     console.log(error);
@@ -110,7 +115,7 @@ async function adminRequest (req, res) {
   }
 }
 
-async function adminApprove (req, res) {
+async function adminApprove(req, res) {
   try {
     const { konten_id } = req.body;
     let user_id;
@@ -119,16 +124,43 @@ async function adminApprove (req, res) {
     } else {
       return res.status(440).send("Login session expired");
     }
-
-    if (result.rows.length === 0 || result.rows[0].roles !== 'Admin') {
+    result = await pool.query("SELECT * FROM USERS WHERE USER_ID = $1", [
+      user_id,
+    ]);
+    if (result.rows.length === 0 || result.rows[0].roles !== "Admin") {
       return res.status(403).send("Access denied: Admins only");
     }
+    const tahap_id = await pool.query(
+      "SELECT * FROM KONTEN INNER JOIN TAHAP ON KONTEN.TAHAP_ID = TAHAP.ID WHERE KONTEN_ID = $1",
+      [konten_id]
+    );
+    const currentTahap = tahap_id.rows[0].nama_tahap;
+    const tahapMap = {
+      "Quality Control": "Design",
+      "Design": "Ready to Publish",
+      "Ready to Publish": "Published",
+    };
 
-    
+    const nextTahap = tahapMap[currentTahap];
+    if (!nextTahap) {
+      return res.status(400).send("Invalid tahap name");
+    }
+    const nextTahapID = await pool.query(
+      "SELECT * FROM TAHAP WHERE NAMA_TAHAP = $1",
+      [nextTahap]
+    );
+    const updated = await pool.query(
+      "UPDATE KONTEN SET TAHAP_ID = $1 WHERE KONTEN_ID = $2 RETURNING *",
+      [nextTahapID.rows[0].id, konten_id]
+    );
+    if (updated.rows.length == 0) {
+      return res.status(500).send("Failed to Update");
+    }
+      return res.status(200).send("Updated Successfuly");
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
   }
 }
 
-module.exports = { adminLogin, adminGrant, adminRequest, adminApprove };
+module.exports = { adminLogin, adminGrant, adminGetRequest, adminApprove };
